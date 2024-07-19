f92c47e2e3f717b3748c0bcefc709e31747a5ea8
JeandreoFurquim <jeandreofur@gmail.com>
2024-07-19 15:34:09 -0300

    First Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html class="h-full" data-theme="true" data-theme-mode="light" lang="en">
+    @include('includes.head')
+    <body class="flex h-full demo1 sidebar-fixed header-fixed bg-[#fefefe] dark:bg-coal-500">
+        <!--begin::Theme mode setup on page load-->
+        @include('includes.config')
+        <!--end::Theme mode setup on page load-->
+        <!--begin::Page layout-->
+        <div class="flex grow">
+            @include('includes.sidebar')
+            <div class="wrapper flex grow flex-col">
+                @include('includes.header')
+                <main class="grow content pt-5" id="content" role="content">
+                    <!-- begin: container -->
+                    <div class="container-fixed" id="content_container">
+                    </div>
+                    <!-- end: container -->
+                    <!-- begin: container -->
+                    <div class="container-fixed">
+                        <div class="flex flex-wrap items-center lg:items-end justify-between gap-5 pb-7.5">
+                            <div class="flex flex-col justify-center gap-2">
+                                <h1 class="text-xl font-semibold leading-none text-gray-900">
+                                    Dashboard
+                                </h1>
+                                <div class="flex items-center gap-2 text-sm font-medium text-gray-600">
+                                    Central Hub for Personal Customization
+                                </div>
+                            </div>
+                            <div class="flex items-center gap-2.5">
+                                <a class="btn btn-sm btn-light" href="html/demo1/public-profile/profiles/default.html">
+                                View Profile
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end: container -->
+                    <!-- begin: container -->
+                    <div class="container-fixed">
+                        <div class="grid gap-5 lg:gap-7.5">
+                            @yield('content')
+                        </div>
+                    </div>
+                    <!-- end: container -->
+                </main>
+                @include('includes.footer')
+            </div>
+        </div>
+        <!--end::Page layout-->
+        <!--begin::Page scripts-->
+        <script src="{{ asset('assets/js/core.bundle.js') }}"></script>
+        <script src="{{ asset('assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+        <script src="{{ asset('assets/js/widgets/general.js') }}"></script>
+        <script src="{{ asset('assets/js/layouts/demo1.js') }}"></script>
+        <!--end::Page scripts-->
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Cadastro - Sistemas Operacionais
+Route::prefix('sistemas-operacionais')->group(function () {
+    Route::name('operational.systems.')->group(function () {
+
+        // Permissão: Visualizar
+        Route::middleware(['allow:show'])->group(function () {
+            Route::get('/', [OperationalSystemsController::class, 'index'])->name('index');
+        });
+
+        // Permissão: Criar
+        Route::middleware(['allow:create'])->group(function () {
+            Route::get('/adicionar', [OperationalSystemsController::class, 'create'])->name('create');
+            Route::post('/adicionar', [OperationalSystemsController::class, 'store'])->name('store');
+        });
+
+        // Permissão: Editar
+        Route::middleware(['allow:edit'])->group(function () {
+            Route::get('/editar/{id}', [OperationalSystemsController::class, 'edit'])->name('edit');
+            Route::put('/editar/{id}', [OperationalSystemsController::class, 'update'])->name('update');
+        });
+
+        // Permissão: Desabilitar
+        Route::middleware(['allow:disable'])->group(function () {
+            Route::get('/desabilitar/{id}', [OperationalSystemsController::class, 'destroy'])->name('destroy');
+        }); 
+    });
+});
+
+require __DIR__.'/auth.php';
